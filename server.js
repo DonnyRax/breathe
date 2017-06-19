8a6e3d86151179e67aa25e326c884ca1567786a8
DonnyRax <rakesh.chotalia@gmail.com>
2017-06-19 08:21:25 +0100

    Update server.js
    Port fix for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 //static - specify folder name to expose
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-	console.log('Express started on port: 3000 ');
+app.listen(PORT, function(){
+	console.log('Express started on port: ' + PORT);
 })
